8f5f3b258d07cfaefcbfa606819ba7322435fdeb
unknown <mariamatef226@gmail.com>
2023-07-07 17:35:01 +0300

    supported text in 6 positions

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,12 +1,15 @@
-import memesData from "../memesData.js";
 import { useState } from "react";
 import { useEffect } from "react";
 
 export default function Meme() {
   
   let [meme, setMeme] = useState({
-    topText: "Text at the top",
-    bottomText: "Text at the bottom",
+    topText: "",
+    bottomText: "",
+    topLeftText: "",
+    bottomLeftText: "",
+    topRightText: "",
+    bottomRightText: "",
     randomImage: "https://i.imgflip.com/30b1gx.jpg",
   });
 
@@ -20,7 +23,6 @@ export default function Meme() {
       });
   }, []);
 
- 
 
   function getMemeImage() {
     let ind = Math.round(Math.random(0, 1) * allMemes.length);
@@ -51,6 +53,7 @@ export default function Meme() {
               value={meme.topText}
               onChange={changeHandler}
               name="topText"
+              placeholder="Text at the top center"
             />
           </div>
 
@@ -61,6 +64,57 @@ export default function Meme() {
               value={meme.bottomText}
               onChange={changeHandler}
               name="bottomText"
+              placeholder="Text at the bottom center"
+
+            />
+          </div>
+          
+          <div className="col-md-6">
+            <input
+              type="text"
+              className="form-control"
+              value={meme.topLeftText}
+              onChange={changeHandler}
+              name="topLeftText"
+              placeholder="Text at the top left"
+
+            />
+          </div>
+                    
+          <div className="col-md-6">
+            <input
+              type="text"
+              className="form-control"
+              value={meme.topRightText}
+              onChange={changeHandler}
+              name="topRightText"
+              placeholder="Text at the top right"
+
+            />
+          </div>
+
+          <div className="col-md-6">
+            <input
+              type="text"
+              className=" form-control "
+              value={meme.bottomLeftText}
+              onChange={changeHandler}
+              name="bottomLeftText"
+              placeholder="Text at the bottom left"
+
+            />
+          </div>
+
+
+          <div className="col-md-6">
+            <input
+              type="text"
+              className=" form-control "
+              value={meme.bottomRightText}
+              onChange={changeHandler}
+              name="bottomRightText"
+              placeholder="Text at the bottom right"
+
             />
           </div>
 
@@ -72,8 +126,12 @@ export default function Meme() {
 
       <div className="meme img-container ms-auto me-auto">
         <img src={meme.randomImage} className="meme--image" />
-        <h2 className="meme--text top">{meme.topText}</h2>
-        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        <h2 className="meme-text top">{meme.topText}</h2>
+        <h2 className="meme-text bottom">{meme.bottomText}</h2>
+        <h2 className="meme-text topLeft">{meme.topLeftText}</h2>
+        <h2 className="meme-text bottomLeft">{meme.bottomLeftText}</h2>
+        <h2 className="meme-text topRight">{meme.topRightText}</h2>
+        <h2 className="meme-text bottomRight">{meme.bottomRightText}</h2>
       </div>
 
       {/*}
