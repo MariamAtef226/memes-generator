cad79cbbccb1ec67dc50f1a28637bf99c3bf27b8
unknown <mariamatef226@gmail.com>
2023-07-07 15:18:11 +0300

    finished development

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,47 +3,84 @@ import { useState } from "react";
 
 export default function Header() {
   // let [memeImage, setMemeImage] = useState("https://i.imgflip.com/30b1gx.jpg");
-  
+
   let [meme, setMeme] = useState({
-    topText:"",
-    bottomText:"",
-    randomImage:"https://i.imgflip.com/30b1gx.jpg"
-  })
+    topText: "Text at the top",
+    bottomText: "Text at the bottom",
+    randomImage: "https://i.imgflip.com/30b1gx.jpg",
+  });
 
-  let [allMemeImages, setAllMemeImages] = useState(memesData)
+  let [allMemeImages, setAllMemeImages] = useState(memesData);
 
+  
   function getMemeImage() {
     let ind = Math.round(Math.random(0, 1) * 100);
-    setMeme(prev =>({ ...prev, randomImage: allMemeImages.data.memes[ind].url}));
+    setMeme((prev) => ({
+      ...prev,
+      randomImage: allMemeImages.data.memes[ind].url,
+    }));
+    window.open("", document.getElementById("ss").toDataURL());
   }
+
+  function changeHandler(event) {
+    let { name, value } = event.target;
+    setMeme((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
+
+
 
   return (
     <>
       <div className="d-flex align-items-center p-3 form-container ms-auto me-auto">
         <div className="row g-3 w-100 justify-content-center ms-auto me-auto">
+         
           <div className="col-md-6">
             <input
               type="text"
               className="form-control"
-              value="Text at the top"
+              value={meme.topText}
+              onChange={changeHandler}
+              name="topText"
             />
           </div>
+         
           <div className="col-md-6">
             <input
               type="text"
               className=" form-control "
-              value="Text at the bottom"
+              value={meme.bottomText}
+              onChange={changeHandler}
+              name="bottomText"
             />
           </div>
+         
           <button onClick={getMemeImage} className="form-control">
             Get a new meme image 🖼
           </button>
+        
         </div>
       </div>
-      <div className="img-container ms-auto me-auto">
 
-        <img  src={meme.randomImage} alt="meme-image"/>
+      <div className="meme img-container ms-auto me-auto">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
+
+
+      {/*}
+      <div>
+        <button>
+          Download
+          https://stackoverflow.com/questions/52620913/how-to-convert-my-html-div-to-canvas-without-using-html2canvas*
+        </button>
+      </div>*/}
+
     </>
   );
 }
